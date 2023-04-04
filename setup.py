673317d67415197be4d7df83818d48ad675c8ee4
Wagh-09 <kirtiwagh09@gmail.com>
2023-04-04 23:42:22 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="census-income",
+      version="0.0.1",
+      author="kirti",
+      author_email="kirtiwagh09@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"])
